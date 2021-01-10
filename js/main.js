55f5b3feefbe19110ebcb320410658f13dffb401
Górka Mateusz <maatiug@gmail.com>
2021-01-10 14:54:14 +0100

    only stared repos

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const svg_fork='<svg class="svg fork" viewBox="0 0 16 16" version="1.1" width="1
 const svg_star='<svg class="svg star" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>'
 
 class Repo {
+    static is_stared( repo_data ){
+        return repo_data.stargazers_count > 0
+    }
+
     constructor( data )
     { this.data = data }
 
@@ -19,13 +23,19 @@ class Repo {
 
     icon(){
         if( this.data.fork == true ) return svg_fork
-        else if( this.data.stargazers_count > 0 ) return svg_star
+        else if( this.data.stargazers_count > 1 ) return svg_star
         else return ""
     }
 
     static get_repo_list( data ){
         var repos_list = []
         data.forEach( repo_data => repos_list.push( (new Repo( repo_data )).echo() ) )
+        return repos_list
+    }
+
+    static get_stared_repo_list( data ){
+        var repos_list = []
+        data.forEach( repo => {if(Repo.is_stared(repo)) repos_list.push((new Repo(repo)).echo())})
         return repos_list
     }
 }
@@ -48,7 +58,7 @@ class GithubUser {
             this.get("repos_url"),
             function( data ){
                 var $repos_cont = $( DOM_id )
-                var repos_list = Repo.get_repo_list( data )
+                var repos_list = Repo.get_stared_repo_list( data )
 
                 repos_list.forEach( function( repo, id ){
                     $repos_cont.append( repo )
