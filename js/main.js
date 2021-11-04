80cd8a78961fe430c6abb51d49d7f2ea1ad1e65d
Górka Mateusz <maatiug@gmail.com>
2021-11-04 19:22:25 +0100

    test for content-url

--- js/main.js
+++ js/main.js
@@ -32,7 +32,6 @@ class Repo {
     }
 
     static compare(a, b) {
-        console.log(a, b)
         var ans = Repo._compare(a.data.stargazers_count, b.data.stargazers_count)
         if (ans == 0) ans = Repo._compare(a.data.pushed_at, b.data.pushed_at)
         return ans
@@ -97,12 +96,21 @@ class GithubUser {
     }
 }
 
+const loadFromMikrus = (domain) => {
+    $.support.cors = true;
+    $.ajax({
+        url: domain + "test",
+        dataType: "text",
+        success: (data) => {
+            console.log(data)
+        }
+    })
+}
 
-function inc_stat() {
-    if (document.location.href.split("//")[1].split(":")[0] === "127.0.0.1")
-        console.log("Local")
-    else
-        $.get("https://bit.ly/3p6iVZS")
+const initContentLoading = () => {
+    $.get("/config/content-url", (data) => {
+        loadFromMikrus(data)
+    })
 }
 
 
@@ -120,8 +128,8 @@ $(document).ready(function () {
     )
 
     $(".social").each(function (id) {
-        $(this).css("display", "flex").hide().delay(150 * id).fadeIn(2000)
+        $(this).css("display", "flex").hide().delay(150 * id).fadeIn(5000)
     })
 
-    inc_stat()
+    initContentLoading()
 })
