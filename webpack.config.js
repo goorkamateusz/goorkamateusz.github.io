609c1d646d7aaed3ef8b1308a6a6c571f1718e20
Górka Mateusz <maatiug@gmail.com>
2022-04-20 21:24:26 +0200

    Migrate React to TS

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        plugins: [new TsconfigPathsPlugin()],
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') })]
+};
